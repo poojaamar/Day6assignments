b2dd4b9f8e7032e8fee6f754890d6fe0c6554cd7
poojaamar <pamar6401@gmail.com>
2022-07-18 23:41:06 +0530

    prob1 fabonancy series

---
+++ Day6LogicalProblem/Day6LogicalProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = 0, num2 = 1, num3;
+Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine()); 
+Console.WriteLine(num1);
+for (int i = 2; i <= num; i++)  
+{
+    num3 = num1 + num2; 
+    Console.WriteLine(num3);
+    num1 = num2;
+    num2 = num3;
+}
