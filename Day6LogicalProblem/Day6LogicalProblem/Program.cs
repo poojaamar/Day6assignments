d1cb327dc11ac4e1987d0f34ca7bc52704cb121b
poojaamar <pamar6401@gmail.com>
2022-07-19 00:08:37 +0530

    generate coupan number

--- Day6LogicalProblem/Day6LogicalProblem/Program.cs
+++ Day6LogicalProblem/Day6LogicalProblem/Program.cs
@@ -1,19 +1,41 @@
-﻿Console.Write("Enter the number: ");
-int num = int.Parse(Console.ReadLine()); 
-int originalNum = num; 
-int palin = 0, ans;
-while (num > 0) 
+﻿static Boolean isPresent(int[] arr, int num) //this method checks whether the no is already present in array or not
 {
-    ans = num % 10;
-    palin = palin * 10 + ans; 
-    num /= 10; 
+    for (int i = 0; i < arr.Length; i++) // for loop is used so we can compare the value present in num with each value present in array
+    {
+        if (arr[i] == num)  //if this condition becomes true it means that num is present in array and will return value true
+        {
+            return true;
+        }
+    }
+    return false;
 }
-if (originalNum == palin) 
+static void generateCouponCode(int totalNum)
 {
-    Console.WriteLine("Number is Palindrome");
+    int[] couponCode = new int[totalNum];  //creating a array to store the distinct coupon code
+    int randomNo = 0, count = 0;
+    Boolean check;
+    Random random = new Random();
+    for (int i = 0; i < couponCode.Length; i++) // for loop is used to generate n no of random number
+    {
+        randomNo = random.Next(1000, 10000);
+        count++;
+        check = isPresent(couponCode, randomNo); //the value true or false return from isPresent function is store in check variable
+        if (check != true) 
+        {
+            couponCode[i] = randomNo;
+        }
+        else
+        {
+            --i; 
+        }
+    }
+    Console.WriteLine("The unique coupon numbers are :");
+    for (int i = 0; i < couponCode.Length; i++) 
+    {
+        Console.WriteLine(couponCode[i] + " ");
+    }
+    Console.WriteLine("Total random number needed to have all distinct numbers are : " + count);
 }
-else
-{
-    Console.WriteLine("Number is not Palindrome");
-}
-
+Console.Write("Enter the number of coupon codes to be generated : ");
+int totalNum = int.Parse(Console.ReadLine());  
+generateCouponCode(totalNum); 
